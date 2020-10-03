842f773de313faa8a99cc8c1d9bdafc33610962e
Hilman Fathur Rakhmad <hifraofficial@gmail.com>
2020-10-03 16:41:35 +0700

    feedback sub 2

--- src/scripts/utils/register-sw.js
+++ src/scripts/utils/register-sw.js
@@ -3,7 +3,7 @@ import { Workbox } from 'workbox-window';
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     const workbox = new Workbox('../service-worker.js');
-    workbox.register();
+    await workbox.register();
   }
 };
 
